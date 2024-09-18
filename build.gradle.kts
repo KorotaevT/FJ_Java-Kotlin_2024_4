c8f5012d3a32cd7f4df6583eba91765522ae3d7d
KorotaevT <korotaevtimofey@gmail.com>
2024-09-18 16:48:30 +0300

    feature(fj-4.1): add dependencies and dto

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.10"
+    kotlin("jvm") version "2.0.20"
+    kotlin("plugin.serialization") version "2.0.20"
 }
 
 group = "ru.cs.korotaev"
 version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
 
 repositories {
     mavenCentral()
@@ -13,12 +18,24 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    runtimeOnly("io.ktor:ktor-client-core:2.1.3")
+    implementation("io.ktor:ktor-client-cio-jvm:2.1.3")
+    implementation("io.ktor:ktor-client-serialization-jvm:2.1.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
+    implementation("ch.qos.logback:logback-core:1.5.7")
+    implementation("ch.qos.logback:logback-classic:1.5.7")
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "17"
+}
+
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "17"
 }
